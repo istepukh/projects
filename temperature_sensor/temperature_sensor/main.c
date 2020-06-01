d8bc177ac3eea631cad1c903e19e4775eb9cf638
Ihor Stepukh <ihor.stepukh@globallogic.com>
2020-06-02 00:26:19 +0300

    Initial idea for implement driver

--- temperature_sensor/temperature_sensor/main.c
+++ temperature_sensor/temperature_sensor/main.c
@@ -1,11 +1,60 @@
+#define F_CPU 16000000UL
 #include <avr/io.h>
+#include <util/delay.h>
+#include <stdbool.h>
 
+enum DS18B20_DQ_LEVEL{
+	DS18B20_DQ_HIGH = true,
+	DS18B20_DQ_LOW = false
+};
+	
+void ds18b20_Init(void)
+{
+	DDRB &= ~(1 << DDB0); // as input 
+	PORTB &= ~(1 << PB0); // set pin of port to low level
+}
+
+void ds18b20_Tx(bool level)
+{
+	if (level == 0) {
+		// as output
+		DDRB |= (1 << DDB0);
+	} else {
+		// as input
+		DDRB &= ~(1 << DDB0);
+	}
+}
+
+bool ds18b20_Rx(void)
+{
+	bool pin = PINB & (1 << PINB0);
+	
+	/*reads and outputs the current level
+	  how to read current level? what function to read?*/
+	return pin;
+}
+
+bool ds18b20_InitizalizationSequence(void)
+{
+	bool level;
+	ds18b20_Tx(DS18B20_DQ_LOW);
+	_delay_us(480);
+	ds18b20_Tx(DS18B20_DQ_HIGH);
+	// how to use/implement pull up?
+	_delay_us(70);
+	level = ds18b20_Rx();
+	_delay_us(410);
+	
+	return level;
+}
 
 int main(void)
 {
+	ds18b20_Init();
+	
     while (1) 
     {
-		
+		ds18b20_InitizalizationSequence();
     }
 }
 
