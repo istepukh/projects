718ce46391c88ca5d7e01f135b425fd729e8dbac
Ihor Stepukh <ihor.stepukh@globallogic.com>
2020-06-05 00:09:45 +0300

    Add ReadByte and WriteByte functions

--- temperature_sensor/temperature_sensor/main.c
+++ temperature_sensor/temperature_sensor/main.c
@@ -29,8 +29,6 @@ bool ds18b20_Rx(void)
 {
 	bool pin = PINB & (1 << PINB0);
 	
-	/*reads and outputs the current level
-	  how to read current level? what function to read?*/
 	return pin;
 }
 
@@ -40,7 +38,7 @@ bool ds18b20_InitizalizationSequence(void)
 	ds18b20_Tx(DS18B20_DQ_LOW);
 	_delay_us(480);
 	ds18b20_Tx(DS18B20_DQ_HIGH);
-	// how to use/implement pull up?
+	// implementation of pull up
 	_delay_us(70);
 	level = ds18b20_Rx();
 	_delay_us(410);
@@ -48,13 +46,76 @@ bool ds18b20_InitizalizationSequence(void)
 	return level;
 }
 
+void WriteSlot(bool bit)
+{	
+	if (bit == 1) {
+		// WriteSlot(1);
+		 ds18b20_Tx(DS18B20_DQ_LOW);
+		 _delay_us(5);
+		 ds18b20_Tx(DS18B20_DQ_HIGH);
+		 _delay_us(70);
+	} else {
+		// WriteSlot(0);
+		ds18b20_Tx(DS18B20_DQ_LOW);
+		_delay_us(70);
+		ds18b20_Tx(DS18B20_DQ_HIGH);
+		 
+	}
+}
+
+bool ReadSlot(void)
+{
+	ds18b20_Tx(DS18B20_DQ_LOW);
+	_delay_us(5);
+	ds18b20_Tx(DS18B20_DQ_HIGH);
+	_delay_us(5);
+	bool bit = ds18b20_Rx();
+	_delay_us(70);
+	
+	return bit;
+}
+
+void ds18b20_WriteByte(uint8_t byte)
+{
+	bool bit;
+
+	for (uint8_t i = 0; i < 8; i++){
+		bit = byte & (1 << 0);
+		byte = byte >> 1;
+		
+		WriteSlot(bit);
+	}
+	
+}
+
+uint8_t ds18b20_ReadByte(void)
+{
+	uint8_t byte = 0;
+
+	for (uint8_t i = 0; i < 8; i++) {
+		bool bit = ReadSlot();
+		byte |= (bit << 0);
+		byte = byte << 1; 
+	}
+	
+	return byte;
+}
+
 int main(void)
 {
+	
 	ds18b20_Init();
 	
     while (1) 
     {
 		ds18b20_InitizalizationSequence();
-    }
+		ds18b20_WriteByte(0xCC);
+		ds18b20_WriteByte(0x44);
+		while (ReadSlot() == 0);
+		ds18b20_WriteByte(0xCC);
+		ds18b20_WriteByte(0xBE);
+		//ds18b20_WriteByte(0xB8);
+		//ds18b20_WriteByte(0xB4);
+	}
 }
 
